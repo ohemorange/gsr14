bbfb1a951e55825caac13081548e1e1f0824814b
Ramya Rangan <ramyarangan@college.harvard.edu>
2014-06-20 15:44:12 -0700

    Added admin

---
+++ hack/admin.py
@@ -0,0 +1,8 @@
+from django.contrib import admin
+from hack.models import *
+
+admin.site.register(Resource)
+admin.site.register(Survey)
+admin.site.register(Recommendation)
+admin.site.register(Event)
+
