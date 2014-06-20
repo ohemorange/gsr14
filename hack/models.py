f8a615d0e2b6bfd06ef8352b8ec7a373b1df5b7f
Ramya Rangan <ramyarangan@college.harvard.edu>
2014-06-20 15:27:40 -0700

    Fixed models

--- hack/models.py
+++ hack/models.py
@@ -1,25 +1,25 @@
 from django.db import models
 
-class Survey(models.Model):
-    resources_used = models.ManyToManyField(Resource, related_name='survey', null=True, default=[])
-    rating = models.IntegerField(null=False)
-
-class Event(models.Model):
-    name = models.CharField(max_length=50)
-    description = models.TextField(null=True, blank=True)
-    image = models.ImageField(upload_to='events/%Y/%m/%d')
-    recommendation_1 = models.ForeignKey(Recommendation, null=True, related_name='event')
-    recommendation_2 = models.ForeignKey(Recommendation, null=True, related_name='event')
-    recommendation_3 = models.ForeignKey(Recommendation, null=True, related_name='event')
-    resources = models.ManyToManyField(Resource, related_name='event', null=True, default=[])
-
-class Recommendation(models.Model):
-    resources = models.ManytoManyField(Resource, related_name='recommendation', null=True, default=[])
-    num_volunteers = models.IntegerField(null=False)
-
 class Resource(models.Model):
     name = models.CharField(max_length=50)
     description = models.TextField(null=True, blank=True)
     notes = models.TextField(null=True, blank=True)
     link = models.URLField()
     image = models.ImageField(upload_to='image/%Y/%m/%d')
+
+class Survey(models.Model):
+    resources_used = models.ManyToManyField(Resource, related_name='survey', null=True, default=[])
+    rating = models.IntegerField(null=False)
+
+class Recommendation(models.Model):
+    resources = models.ManyToManyField(Resource, related_name='recommendation', null=True, default=[])
+    num_volunteers = models.IntegerField(null=False)
+
+class Event(models.Model):
+    name = models.CharField(max_length=50)
+    description = models.TextField(null=True, blank=True)
+    image = models.ImageField(upload_to='events/%Y/%m/%d')
+    recommendation_1 = models.ForeignKey(Recommendation, null=True, related_name='event_1')
+    recommendation_2 = models.ForeignKey(Recommendation, null=True, related_name='event_2')
+    recommendation_3 = models.ForeignKey(Recommendation, null=True, related_name='event_3')
+    resources = models.ManyToManyField(Resource, related_name='event', null=True, default=[])
