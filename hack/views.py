5aab2cdebb1fa6909cf81bd60f82e83e47d4ad12
Erica Portnoy <eportnoy@princeton.edu>
2014-06-20 18:06:13 -0400

    note where ml stuff goes

--- hack/views.py
+++ hack/views.py
@@ -22,5 +22,7 @@ def save_update(request, event_id):
     e = get_object_or_404(Event, pk=event_id)
     event_rating = request.POST['rating']
     selected_resources = request.POST['resources']
-    
+    # TODO: do ML stuff here
+    context = {'event_id': event_id}
+    return render(request, 'event.html', context)
     
