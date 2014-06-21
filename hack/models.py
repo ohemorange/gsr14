886ed2f4969e09c9febb836417a203e14be3548a
Erica Portnoy <eportnoy@princeton.edu>
2014-06-21 17:28:42 -0400

    updated models file

--- hack/models.py
+++ hack/models.py
@@ -5,11 +5,7 @@ class Resource(models.Model):
     description = models.TextField(null=True, blank=True)
     notes = models.TextField(null=True, blank=True)
     link = models.URLField()
-    image = models.ImageField(upload_to='image/%Y/%m/%d')
-
-class Survey(models.Model):
-    resources_used = models.ManyToManyField(Resource, related_name='survey', null=True, default=[])
-    rating = models.IntegerField(null=False)
+    # image = models.ImageField(upload_to='image/%Y/%m/%d')
 
 class Recommendation(models.Model):
     resources = models.ManyToManyField(Resource, related_name='recommendation', null=True, default=[])
@@ -18,8 +14,13 @@ class Recommendation(models.Model):
 class Event(models.Model):
     name = models.CharField(max_length=50)
     description = models.TextField(null=True, blank=True)
-    image = models.ImageField(upload_to='events/%Y/%m/%d')
+    # image = models.ImageField(upload_to='events/%Y/%m/%d')
     recommendation_1 = models.ForeignKey(Recommendation, null=True, blank=True, related_name='event_1')
     recommendation_2 = models.ForeignKey(Recommendation, null=True, blank=True, related_name='event_2')
     recommendation_3 = models.ForeignKey(Recommendation, null=True, blank=True, related_name='event_3')
     resources = models.ManyToManyField(Resource, related_name='event', null=True, default=[])
+
+class Survey(models.Model):
+    resources_used = models.ManyToManyField(Resource, related_name='survey', null=True, default=[])
+    rating = models.IntegerField(null=False)
+    event = models.ForeignKey(Event, related_name='survey_event')
