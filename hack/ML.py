0eefbc03a0ddc5ed9633b3ac198eaa0ef997e7e3
Akanksha <akankshasaran@gmail.com>
2014-06-20 17:38:35 -0700

    added ML.py

---
+++ hack/ML.py
@@ -0,0 +1,11 @@
+#the machine learning stuff herei
+import numpy as np
+
+all_entries = Survey.objects.all()
+count = Survey.objects.all().count()
+resources_used = all_entries.resources_used
+matrix_ratings = np.zeros(count,1)
+matrix_coefficients = np.zeros(count,1)
+matrix_binaryFeatures = np.zeros(count,resource_count)
+for i in range(1,count)
+	resources_used[i]
